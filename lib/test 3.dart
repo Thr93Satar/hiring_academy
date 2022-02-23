443d9784d37dcdebf09602fd152b406154559c0b
UnbrokenDream <thr93satar@gmail.com>
2022-02-23 09:30:23 +0300

    Organizing Shapes

---
+++ lib/test 3.dart
@@ -0,0 +1,188 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget{
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: SafeArea(
+        child: Scaffold(
+          body: Container(
+            color: Colors.white,
+            child: Column(
+                children: [
+                  Expanded(
+                    child: Row( //row 1
+                        children: [
+                          Expanded(
+                            child: Container(
+                              decoration: const BoxDecoration(
+                                color: Colors.red,
+                              ),),
+                          ),
+                          Expanded(child: Container(
+                            decoration: const BoxDecoration(
+                              color: Colors.blue,
+                            ),
+                          ),)
+                        ]
+                    ),
+                  ),
+                  Expanded(
+                    child: Row( //row 2
+                        children: [
+                          Expanded(
+                            flex: 2,
+                            child: Container(
+                              decoration: const BoxDecoration(
+                                color: Colors.blue,
+                              ),),
+                          ),
+                          Expanded(
+                            flex: 6,
+                            child: Container(
+                              decoration: const BoxDecoration(
+                                color: Colors.red,
+                              ),
+                            ),
+                          ),
+                          Expanded(
+                            flex: 2,
+                            child: Container(
+                              decoration: const BoxDecoration(
+                                color: Colors.blue,
+                              ),
+                            ),
+                          ),
+                        ]
+                    ),
+                  ),
+                  Expanded(
+                    child: Row ( //row 3
+                        children: [
+                          Expanded(
+                            flex: 3,
+                            child: Container(
+                              decoration: const BoxDecoration(
+                                color: Colors.red,
+                              ),),
+                          ),
+                          const Spacer(
+                            flex: 4,
+                          ),
+                          Expanded(
+                            flex: 3,
+                            child: Container(
+                              decoration: const BoxDecoration(
+                                color: Colors.blue,
+                              ),
+                            ),
+                          ),
+                        ]
+                    ),
+                  ),
+                  Expanded(
+                    child: Row ( //row 4
+                        children: [
+                          Expanded(
+                            flex: 5,
+                            child: Container(
+                              margin: const EdgeInsets.symmetric(horizontal: 100),
+                              decoration: const BoxDecoration(
+                                color: Colors.green,
+                              ),
+                            ),
+                          ),
+                        ]
+                    ),
+                  ),
+                  Expanded(
+                    child: Row (//row 5
+                        children: [
+                          Expanded(
+                            flex: 5,
+                            child: Container(
+                              decoration: const BoxDecoration(
+                                color: Colors.yellow,
+                              ),
+                            ),
+                          ),
+                          const Expanded(
+                            flex: 5,
+                            child: SizedBox(
+                            ),
+                          ),
+                        ]
+                    ),
+                  ),
+                  Expanded(
+                    child: Row ( //row 6
+                        children: [
+                          Expanded(
+                            flex: 3,
+                            child: Container(
+                              decoration: const BoxDecoration(
+                                color: Colors.red,
+                              ),
+                            ),
+                          ),
+                          Expanded(
+                              flex: 4,
+                              child: Column(
+                                children: [
+                                  Expanded(
+                                    child: Row(
+                                      crossAxisAlignment: CrossAxisAlignment.start,
+                                      children: [
+                                        Expanded(
+                                          flex: 4,
+                                          child: Container(
+                                            decoration: const BoxDecoration(
+                                              color: Colors.blue,
+                                            ),
+                                          ),
+                                        )
+                                      ],
+                                    ),
+                                  ),
+                                  Expanded(
+                                    child: Row (
+                                      children: [
+                                        Expanded(
+                                          flex: 4,
+                                          child: Container(
+                                            decoration: const BoxDecoration(
+                                              color: Colors.green,
+                                            ),
+                                          ),
+                                        )
+                                      ],
+                                    ),
+                                  )
+                                ],
+
+                              )),
+                          Expanded(
+                            flex: 3,
+                            child: Container(
+                              decoration: const BoxDecoration(
+                                color: Colors.black,
+                              ),
+                            ),
+                          ),
+                        ]
+                    ),
+                  )
+                ]
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
