8954d5376d9afc197431c3dc146150e433bb1c1e
UnbrokenDream <thr93satar@gmail.com>
2022-02-25 02:39:59 +0300

    I Hate my flutter code xD

--- lib/Trash code.dart
+++ lib/Trash code.dart
@@ -1,5 +1,8 @@
-// import 'package:flutter/cupertino.dart';
+import 'dart:ui';
+
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,181 +15,207 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        body: Column(
-            children: [
-              Expanded(
-                child: Stack(
-                    children: [
-                      SizedBox(
-                        height: 400,
-                        width: double.infinity,
-                      ),
-                      Column(
-                          mainAxisAlignment: MainAxisAlignment.end,
-                          children: [
-                            Expanded(
+        body: Column(children: [
+          Expanded(
+            child: Stack(children: [
+              Column(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  const SizedBox(
+                    height: 400,
+                  ),
+                  Expanded(
+                    flex: 2,
+                    child: Row(
+                      //---------------------------------------------first row
+                        children: [
+                          const SizedBox(
+                            width: 20,
+                          ),
+                          Expanded(
+                            flex: 1,
+                            child: Stack(children: [
+                              Container(
+                                decoration: const BoxDecoration(
+                                    color: Colors.red, shape: BoxShape.circle),
+                              ),
+                              Container(
+                                  decoration: const BoxDecoration(
+                                      color: Colors.red,
+                                      borderRadius: BorderRadius.horizontal(
+                                          right: Radius.circular(5))),
+                                  child: const Icon(Icons.attach_money)),
+                              Container(
+                                  child: const Text(
+                                    '68',
+                                  ))
+                            ]),
+                          ),
+                          Expanded(
                               flex: 2,
-                              child: Row(// ------------------------- Pricing and Like
-                                  children: [
-                                    Expanded(
-                                      flex: 3,
-                                      child: Container(
-                                        decoration: const BoxDecoration(
-                                          color: Colors.red,
-                                        ),
-                                      ),
-                                    ),
-                                    Expanded(
-                                        flex: 4,
-                                        child: Column(
-                                          children: [
-                                            Expanded(
-                                              child: Row(
-                                                crossAxisAlignment: CrossAxisAlignment.start,
-                                                children: [
-                                                  Expanded(
-                                                    flex: 4,
-                                                    child: Container(
-                                                      decoration: const BoxDecoration(
-                                                        color: Colors.blue,
-                                                      ),
-                                                    ),
-                                                  )
-                                                ],
+                              child: Column(
+                                children: [
+                                  Expanded(
+                                    child: Row(
+                                      crossAxisAlignment:
+                                      CrossAxisAlignment.start,
+                                      children: [
+                                        Expanded(
+                                          flex: 1000,
+                                          child: Container(
+                                            child: const Text(
+                                              'PP-0008',
+                                              textAlign: TextAlign.center,
+                                              style: TextStyle(
+                                                fontSize: 20,
+                                                height: 2.5,
+                                                fontWeight: FontWeight.w800,
                                               ),
                                             ),
-                                            Expanded(
-                                              child: Row(
-                                                children: [
-                                                  Expanded(
-                                                    flex: 4,
-                                                    child: Container(
-                                                      decoration: const BoxDecoration(
-                                                        color: Colors.green,
-                                                      ),
-                                                    ),
-                                                  )
-                                                ],
-                                              ),
-                                            )
-                                          ],
-                                        )),
-                                    Expanded(
-                                      flex: 3,
-                                      child: Container(
-                                        decoration: const BoxDecoration(
-                                          color: Colors.black,
-                                        ),
-                                      ),
-                                    ),
-                                  ]),
-                            ),
-                            Expanded(
-                              flex : 2,
-                              child: Row(
-                                mainAxisAlignment: MainAxisAlignment.center,
-                                children: [
-                                  Container(
-                                    width: 300,
-                                    decoration: const BoxDecoration(
-                                      color: Colors.red,
-                                    ),
-                                    child: const Text(
-                                      'Montreal-Based foundry, Pangram, has been a disrupter in the typogtaphy world since 2016',
-                                      textAlign: TextAlign.start,
-                                      style: TextStyle(
-                                        fontSize: 18,
-                                        color: Colors.black54,
-                                      ),
+                                          ),
+                                        )
+                                      ],
                                     ),
                                   ),
+                                  Expanded(
+                                    child: Row(
+                                      children: [
+                                        Expanded(
+                                          flex: 1000,
+                                          child: Container(
+                                            child: const Text(
+                                              '       Free Shipping',
+                                              textAlign: TextAlign.justify,
+                                              style: TextStyle(
+                                                  color: Colors.black54,
+                                                  fontSize: 18,
+                                                  height: 0.2),
+                                            ),
+                                            alignment: Alignment.center,
+                                          ),
+                                        )
+                                      ],
+                                    ),
+                                  )
                                 ],
+                              )),
+                          Expanded(
+                            flex: 1,
+                            child: Container(
+                              child: const Icon(
+                                Icons.favorite,
+                                size: 30,
                               ),
                             ),
-                            Expanded( //------------------------------------------------------ 3rd card
-                              child: Row(
-                                children: [
-                                  Expanded(
-                                      flex: 2,
-                                      child: Container(
-                                        decoration: const BoxDecoration(
-                                          color: Colors.blue,
-                                        ),
-                                      )
-                                  ),
-                                  Expanded(
-                                      flex: 2,
-                                      child: Container(
-                                        decoration: const BoxDecoration(
-                                          color: Colors.red,
-                                        ),
-                                      )
-                                  ),
-                                  Expanded(
-                                      flex: 2,
-                                      child: Container(
-                                        decoration: const BoxDecoration(
-                                          color: Colors.green,
-                                        ),
-                                      )
-                                  ),
-                                  Expanded(
-                                      flex: 2,
-                                      child: Container(
-                                        decoration: const BoxDecoration(
-                                          color: Colors.yellow,
-                                        ),
-                                      )
-                                  ),
-
-                                ],
-                              ),
+                          ),
+                        ]),
+                  ),
+                  Expanded(
+                    flex: 2,
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Container(
+                          width: 300,
+                          decoration: const BoxDecoration(),
+                          child: const Text(
+                            'Montreal-Based foundry, Pangram, has been a disrupter in the typogtaphy world since 2016',
+                            textAlign: TextAlign.start,
+                            style: TextStyle(
+                              fontSize: 18,
+                              color: Colors.black54,
                             ),
-                            Expanded(   // ------------------------------------------------- last card
-                              child: Row(
-                                  children: [
-                                    Expanded(
-                                        flex: 2,
-                                        child: Container(
-                                          decoration: const BoxDecoration(
-                                            color: Colors.blue,
-                                          ),
-                                        )
-                                    ),
-                                    Expanded(
-                                        flex: 2,
-                                        child: Container(
-                                          decoration: const BoxDecoration(
-                                            color: Colors.red,
-                                          ),
-                                        )
-                                    ),
-                                    Expanded(
-                                        flex: 2,
-                                        child: Container(
-                                          decoration: const BoxDecoration(
-                                            color: Colors.green,
-                                          ),
-                                        )
-                                    ),
-                                    Expanded(
-                                        flex: 2,
-                                        child: Container(
-                                          decoration: const BoxDecoration(
-                                            color: Colors.yellow,
-                                          ),
-                                        )
-                                    ),
-                                  ]
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Expanded(
+                    //------------------------------------------------------ 3rd card
+                    flex: 2,
+                    child: Row(
+                      children: [
+                        Expanded(
+                            flex: 2,
+                            child: Container(
+                              decoration: const BoxDecoration(
+                                color: Colors.blue,
                               ),
-                            ),
-                          ]
-                      ),
-                      Image.asset('assets/pexels-timothy-paule-ii-2002717.jpg'),
-                    ]
+                            )),
+                        Expanded(
+                            flex: 2,
+                            child: Container(
+                              decoration: const BoxDecoration(
+                                color: Colors.red,
+                              ),
+                            )),
+                        Expanded(
+                            flex: 2,
+                            child: Container(
+                              decoration: const BoxDecoration(
+                                color: Colors.green,
+                              ),
+                            )),
+                        Expanded(
+                            flex: 2,
+                            child: Container(
+                              decoration: const BoxDecoration(
+                                color: Colors.yellow,
+                              ),
+                            )),
+                      ],
+                    ),
+                  ),
+                  Expanded(
+                    flex: 2,
+                    //------------------------------------------------------ 3rd card
+                    child: Row(
+                      children: [
+                        Expanded(
+                            flex: 2,
+                            child: Container(
+                              decoration: const BoxDecoration(
+                                color: Colors.blue,
+                              ),
+                            )),
+                        Expanded(
+                            flex: 2,
+                            child: Container(
+                              decoration: const BoxDecoration(
+                                color: Colors.red,
+                              ),
+                            )),
+                        Expanded(
+                            flex: 2,
+                            child: Container(
+                              decoration: const BoxDecoration(
+                                color: Colors.green,
+                              ),
+                            )),
+                        Expanded(
+                            flex: 2,
+                            child: Container(
+                              decoration: const BoxDecoration(
+                                color: Colors.yellow,
+                              ),
+                            )),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+              SizedBox(
+                height: 400,
+                width: double.infinity,
+                child: Image.asset(
+                  'assets/pexels-timothy-paule-ii-2002717.jpg',
+                  fit: BoxFit.fitWidth,
+                  alignment: Alignment.topCenter,
                 ),
               ),
             ]),
+          ),
+        ]),
       ),
     );
   }
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
-// import 'package:flutter/cupertino.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,112 +13,107 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        body: Column(
-            children: [
-              Expanded(
-                child: Stack(
-                  children: [
-                    Expanded(
-                      flex: 5,
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.end,
-                        children: [
-                          SizedBox(
-                            height: 400,
-                          ),
-                          Expanded(
-                            child: Row ( //row 6
-                                children: [
-                                  Expanded(
-                                    flex: 3,
-                                    child: Container(
-                                      decoration: const BoxDecoration(
-                                        color: Colors.red,
-                                      ),
-                                    ),
-                                  ),
-                                  Expanded(
-                                      flex: 4,
-                                      child: Column(
-                                        children: [
-                                          Expanded(
-                                            child: Row(
-                                              crossAxisAlignment: CrossAxisAlignment.start,
-                                              children: [
-                                                Expanded(
-                                                  flex: 4,
-                                                  child: Container(
-                                                    decoration: const BoxDecoration(
-                                                      color: Colors.blue,
-                                                    ),
-                                                  ),
-                                                )
-                                              ],
+        body: Column(children: [
+          Expanded(
+            child: Stack(
+              children: [
+                Expanded(
+                  child: Column (
+                    children: [
+                      Expanded(
+                        flex: 2,
+                        child: Row(
+                          children: [
+                            Expanded(
+                              child: Container (
+                                decoration: BoxDecoration(
+                                  color: Colors.yellow,
+                                  borderRadius: BorderRadius.only(topRight: Radius.circular(20),topLeft: Radius.circular(20))
+                                ),
+                                child: Expanded( //-------------------------------------------------First Card----------------------------------
+                                  child: Row (
+                                      children: [
+                                        Expanded(
+                                          flex: 3,
+                                          child: Container(
+                                            decoration: const BoxDecoration(
+                                              color: Colors.red,
                                             ),
                                           ),
-                                          Expanded(
-                                            child: Row (
+                                        ),
+                                        Expanded(
+                                            flex: 4,
+                                            child: Column(
                                               children: [
                                                 Expanded(
-                                                  flex: 4,
-                                                  child: Container(
-                                                    decoration: const BoxDecoration(
-                                                      color: Colors.green,
-                                                    ),
+                                                  child: Row(
+                                                    crossAxisAlignment: CrossAxisAlignment.start,
+                                                    children: [
+                                                      Expanded(
+                                                        flex: 4,
+                                                        child: Container(
+                                                          decoration: const BoxDecoration(
+                                                            color: Colors.blue,
+                                                          ),
+                                                        ),
+                                                      )
+                                                    ],
+                                                  ),
+                                                ),
+                                                Expanded(
+                                                  child: Row (
+                                                    children: [
+                                                      Expanded(
+                                                        flex: 4,
+                                                        child: Container(
+                                                          decoration: const BoxDecoration(
+                                                            color: Colors.green,
+                                                          ),
+                                                        ),
+                                                      )
+                                                    ],
                                                   ),
                                                 )
                                               ],
-                                            ),
-                                          )
-                                        ],
 
-                                      )),
-                                  Expanded(
-                                    flex: 3,
-                                    child: Container(
-                                      decoration: const BoxDecoration(
-                                        color: Colors.black,
-                                      ),
-                                    ),
-                                  ),
-                                ]
-                            ),
-                          ),
-                          Expanded(
-                            flex : 2,
-                            child: Row(
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              children: [
-                                Container(
-                                  width: 300,
-                                  decoration: const BoxDecoration(
-                                    color: Colors.red,
-                                  ),
-                                  child: const Text(
-                                    'Montreal-Based foundry, Pangram, has been a disrupter in the typogtaphy world since 2016',
-                                    textAlign: TextAlign.start,
-                                    style: TextStyle(
-                                      fontSize: 18,
-                                      color: Colors.black54,
-                                    ),
+                                            )),
+                                        Expanded(
+                                          flex: 3,
+                                          child: Container(
+                                            decoration: const BoxDecoration(
+                                              color: Colors.black,
+                                            ),
+                                          ),
+                                        ),
+                                      ]
                                   ),
                                 ),
-                              ],
-                            ),
-                          ),
-                        ],
+                              ),
+                            ) //---------------------------------------------------------End of first Card
+                          ],
+                        ),
                       ),
-                    ),
-                    SizedBox(
-                      height: 400,
-                      width: double.infinity,
-                      child: Image.asset('assets/pexels-timothy-paule-ii-2002717.jpg', fit: BoxFit.fitWidth , alignment: Alignment.topCenter,),
-                    ),
-                  ]
+                      Expanded(
+                        flex: 3,
+                        child: Row(
+                        ),
+                      ),
+                    ],
+                  ),
                 ),
-              ),
-        ]
-        ),
+                // SizedBox(
+                //   height: 400,
+                //   width: double.infinity,
+                //   child: Image.asset(
+                //     'assets/pexels-timothy-paule-ii-2002717.jpg',
+                //     fit: BoxFit.fitWidth,
+                //     alignment: Alignment.topCenter,
+                //   ),
+                // ),
+              ],
+            ),
+          )
+        ]),
       ),
     );
   }
