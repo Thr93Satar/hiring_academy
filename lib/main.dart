39417d0da07a9f72ae00bc6881cac8d67d2dbfaa
UnbrokenDream <thr93satar@gmail.com>
2022-02-24 19:11:02 +0300

    Organizing Shapes V2

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+// import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -10,181 +11,175 @@ class MyApp extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: SafeArea(
-        child: Scaffold(
-          body: Container(
-            color: Colors.white,
-            child: Column(
-              children: [
-                Expanded(
-                  child: Row( //row 1
+      home: Scaffold(
+        body: Column(
+          children: [
+            Expanded(
+              child: Stack (
+              children:  [
+               Row(
+            children: [
+              Expanded(
+                child: Row ( //row 6
                     children: [
                       Expanded(
+                        flex: 3,
                         child: Container(
-                        decoration: const BoxDecoration(
-                          color: Colors.red,
-                        ),),
+                          decoration: const BoxDecoration(
+                            color: Colors.red,
+                          ),
+                        ),
                       ),
-                      Expanded(child: Container(
-                        decoration: const BoxDecoration(
-                          color: Colors.blue,
-                        ),
-                      ),)
-                    ]
-                  ),
-                ),
-                Expanded(
-                  child: Row( //row 2
-                      children: [
-                        Expanded(
-                          flex: 2,
-                          child: Container(
-                            decoration: const BoxDecoration(
-                              color: Colors.blue,
-                            ),),
-                        ),
-                        Expanded(
+                      Expanded(
                           flex: 4,
-                          child: Container(
-                            decoration: const BoxDecoration(
-                              color: Colors.red,
-                            ),
-                          ),
-                        ),
-                        Expanded(
-                          flex: 2,
-                          child: Container(
-                            decoration: const BoxDecoration(
-                              color: Colors.blue,
-                            ),
-                          ),
-                        ),
-                      ]
-                  ),
-                ),
-                Expanded(
-                  child: Row ( //row 3
-                      children: [
-                        Expanded(
-                          flex: 3000,
-                          child: Container(
-                            decoration: const BoxDecoration(
-                              color: Colors.red,
-                            ),),
-                        ),
-                        const Spacer(
-                          flex: 4000,
-                        ),
-                        Expanded(
-                          flex: 3000,
-                          child: Container(
-                            decoration: const BoxDecoration(
-                              color: Colors.red,
-                            ),
-                          ),
-                        ),
-                      ]
-                  ),
-                ),
-                Expanded(
-                  child: Row ( //row 4.1
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                      children: [
-                        const Spacer(flex: 2500,),
-                        Expanded(
-                          flex: 5000,
-                          child: Container(
-                            decoration: const BoxDecoration(
-                              color: Colors.green,
-                            ),
-                          ),
-                        ),
-                        const Spacer(flex: 2500,),
-                      ]
-                  ),
-                ),
-                Expanded(
-                  child: Row (//row 5
-                      children: [
-                        Expanded(
-                          flex: 5,
-                          child: Container(
-                            decoration: const BoxDecoration(
-                              color: Colors.yellow,
-                            ),
-                          ),
-                        ),
-                        const Expanded(
-                          flex: 5,
-                          child: SizedBox(
-                          ),
-                        ),
-                      ]
-                  ),
-                ),
-                Expanded(
-                  child: Row ( //row 6
-                    children: [
-                    Expanded(
-                      flex: 3,
-                      child: Container(
-                        decoration: const BoxDecoration(
-                          color: Colors.red,
-                        ),
-                      ),
-                    ),
-                    Expanded(
-                      flex: 4,
-                        child: Column(
-                          children: [
-                            Expanded(
-                              child: Row(
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                children: [
-                                  Expanded(
-                                    flex: 4,
-                                    child: Container(
-                                      decoration: const BoxDecoration(
-                                        color: Colors.blue,
+                          child: Column(
+                            children: [
+                              Expanded(
+                                child: Row(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Expanded(
+                                      flex: 4,
+                                      child: Container(
+                                        decoration: const BoxDecoration(
+                                          color: Colors.blue,
+                                        ),
                                       ),
-                                    ),
-                                  )
-                                ],
+                                    )
+                                  ],
+                                ),
                               ),
-                            ),
-                            Expanded(
-                              child: Row (
-                                children: [
-                                  Expanded(
-                                    flex: 4,
-                                    child: Container(
-                                      decoration: const BoxDecoration(
-                                        color: Colors.green,
+                              Expanded(
+                                child: Row (
+                                  children: [
+                                    Expanded(
+                                      flex: 4,
+                                      child: Container(
+                                        decoration: const BoxDecoration(
+                                          color: Colors.green,
+                                        ),
                                       ),
-                                    ),
-                                  )
-                                ],
-                              ),
-                            )
-                          ],
+                                    )
+                                  ],
+                                ),
+                              )
+                            ],
 
-                        )),
-                    Expanded(
-                      flex: 3,
-                      child: Container(
-                        decoration: const BoxDecoration(
-                          color: Colors.black,
+                          )),
+                      Expanded(
+                        flex: 3,
+                        child: Container(
+                          decoration: const BoxDecoration(
+                            color: Colors.black,
+                          ),
                         ),
                       ),
-                    ),
                     ]
-                  ),
-                )
-            ]
+                ),
+              )
+            ],
+          ),
+
+
+          //       Column(
+          //         children: [
+          //       Expanded(
+          //         child: Column(
+          //           children: [
+          //             Expanded(
+          //             child: Row ( // ------------------------- Pricing and Like
+          //                 children: [
+          //                   Expanded(
+          //                     flex: 3,
+          //                     child: Container(
+          //                       decoration: const BoxDecoration(
+          //                         color: Colors.red,
+          //                       ),
+          //                     ),
+          //                   ),
+          //                   Expanded(
+          //                       flex: 4,
+          //                       child: Column(
+          //                         children: [
+          //                           Expanded(
+          //                             child: Row(
+          //                               crossAxisAlignment: CrossAxisAlignment.start,
+          //                               children: [
+          //                                 Expanded(
+          //                                   flex: 4,
+          //                                   child: Container(
+          //                                     decoration: const BoxDecoration(
+          //                                       color: Colors.blue,
+          //                                     ),
+          //                                   ),
+          //                                 )
+          //                               ],
+          //                             ),
+          //                           ),
+          //                           Expanded(
+          //                             child: Row (
+          //                               children: [
+          //                                 Expanded(
+          //                                   flex: 4,
+          //                                   child: Container(
+          //                                     decoration: const BoxDecoration(
+          //                                       color: Colors.green,
+          //                                     ),
+          //                                   ),
+          //                                 )
+          //                               ],
+          //                             ),
+          //                           )
+          //                         ],
+          //
+          //                       )),
+          //                   Expanded(
+          //                     flex: 3,
+          //                     child: Container(
+          //                       decoration: const BoxDecoration(
+          //                         color: Colors.black,
+          //                       ),
+          //                     ),
+          //                   ),
+          //                 ]
+          //             ),
+          //           ),
+          //             Expanded(
+          //               child: Row (
+          //                 mainAxisAlignment: MainAxisAlignment.center,
+          //                 children: [
+          //                   Container(
+          //                     width: 300,
+          //                     child: const Text (
+          //                         'Montreal-Based foundry, Pangram, has been a disrupter in the typogtaphy world since 2016',
+          //                       textAlign: TextAlign.start,
+          //                       style: TextStyle(
+          //                         fontSize: 18,
+          //                       color: Colors.black54,
+          //                     ),
+          //                      ),
+          //                   ),
+          //                 ],
+          //               ),
+          //             )
+          //
+          //          ]
+          //         ),
+          //       )
+          //     ],
+          // ),
+                Image.asset(
+                  'assets/pexels-timothy-paule-ii-2002717.jpg',
+                  scale: 9,
+                ),
+          ]
+                ),
+            ),
+          ]
         ),
       ),
-            ),
-          ),
-        );
+    );
+
+
   }
 }
